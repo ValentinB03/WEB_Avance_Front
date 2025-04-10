394af6fae3e3e51fc54018b1ae7b9b026ede6925
valen <valentin.baraffe@viacesi.fr>
2025-04-10 22:38:34 +0200

    Réalisation du service commercial

--- src/ServiceCommercial/ServiceCommercial-info.jsx
+++ src/ServiceCommercial/ServiceCommercial-info.jsx
@@ -1,26 +1,39 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './ServiceCommercial-info.css';
 import NavBar from '../Navbar/NavBar.jsx';
 import Footer from '../Footer/Footer.jsx';
 import ImgBannier from '../assets/img/Service_Commercial.jpg';
 import {Link} from "react-router-dom";
+import {getAllOrder, getRestaurantById, getUser} from "../api/api.jsx";
 
 function ServiceCommercialInfo() {
 
-    const [items] = useState([
-        { id: 1, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 2, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acquittement livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 3, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 4, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 5, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Passation commande', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 6, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Passation commande', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 7, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 8, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 9, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 10, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 11, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acquittement livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-        { id: 12, name: 'Big Bite Burger', Commande: '152-478-965', Etat: 'Acceptation livraison', Date: '20/03/2025 21h21', Prix: '25.00€' },
-    ]);
+    const [orders, setOrders] = useState([]);
+    const [totalTransaction, setTotalTransaction] = useState(0);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const orders = await getAllOrder(); // Récupère toutes les commandes
+            const ordersFilter = orders.filter((item) => item.status !== 'Livrée' && item.status !== 'Annulée');
+            console.log(ordersFilter);
+            const ordersWithUsers = await Promise.all(
+                ordersFilter.map(async (order) => {
+                    const restaurantUser = await getRestaurantById(order.restaurantId); // Récupère les infos du restaurant
+                    const clientUser = await getUser(order.clientId); // Récupère les infos du client
+                    return {
+                        ...order,
+                        restaurantUser,
+                        clientUser,
+                    };
+                })
+            );
+            const total = ordersWithUsers.reduce((acc, order) => acc + parseFloat(order.totalAmount), 0);
+            setOrders(ordersWithUsers);
+            setTotalTransaction(total);
+        }
+        fetchData();
+    } , []);
+
 
     return (
         <div className="App">
@@ -31,23 +44,23 @@ function ServiceCommercialInfo() {
                 <button className={"gestion-compte-bouton"}>Gestion des comptes clients</button>
             </Link>
             <div className="container-sc">
-                <p className={"chiffre-affaire"}>Chiffre d'affaires transactionnel global en cours :</p>
+                <p className={"chiffre-affaire"}>Chiffre d'affaires transactionnel global en cours : {totalTransaction} €</p>
                 <div className="tableau-service-commercial-historique_commandes">
                     <div className="content-service-commercial-historique_commandes">
                         <div className="content-service-commercial-historique_commandes-header">
-                            <div  className={"service-commercial-case-tableau"}>Nom du restaurant</div>
                             <div className={"service-commercial-case-tableau"}>N° Commande</div>
+                            <div  className={"service-commercial-case-tableau"}>Nom du restaurant</div>
                             <div className={"service-commercial-case-tableau"}>Etat de la commande</div>
                             <div className={"service-commercial-case-tableau"}>Dernière modification</div>
                             <div className={"service-commercial-case-tableau"}>Prix</div>
                         </div>
-                        {items.map(item => (
-                            <div key={item.id} className="content-service-commercial-historique_commandes-line">
-                                <p className={"service-commercial-case-tableau"}>{item.name}</p>
-                                <p className={"service-commercial-case-tableau"}>{item.Commande}</p>
-                                <p className={"service-commercial-case-tableau"}>{item.Etat}</p>
-                                <p className={"service-commercial-case-tableau"}>{item.Date}</p>
-                                <p className={"service-commercial-case-tableau"}>{item.Prix}</p>
+                        {orders.map(order => (
+                            <div key={order.id} className="content-service-commercial-historique_commandes-line">
+                                <p className={"service-commercial-case-tableau"}>{order.id}</p>
+                                <p className={"service-commercial-case-tableau"}>{order.restaurantUser.name}</p>
+                                <p className={"service-commercial-case-tableau"}>{order.status}</p>
+                                <p className={"service-commercial-case-tableau"}>{order.updatedAt}</p>
+                                <p className={"service-commercial-case-tableau"}>{order.totalAmount} €</p>
                             </div>
                         ))}
                     </div>
