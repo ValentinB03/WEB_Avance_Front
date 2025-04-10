1ad565c4e138522be2788b4c8ee680de8a718ce2
axelm <axel.mori@viacesi.fr>
2025-04-10 23:24:42 +0200

    notification.jsx

--- src/notification/notification.jsx
+++ src/notification/notification.jsx
@@ -1,21 +1,38 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import {getNotificationByUserId} from "../api/api.jsx";
+
 
 function Notification() {
 
-    const [items] = useState([
-        { id: 1, date: '10/02/2025 15h20', message: 'message notification 1'},
-        { id: 2, date: '10/02/2025 15h25', message: 'message notification 2'},
-        { id: 3, date: '10/02/2025 15h30', message: 'message notification 3'}
-    ]);
+    const [notifications, setNotifications] = useState([]);
+    const userId = JSON.parse(localStorage.getItem('user')).id;
+
+    useEffect(() => {
+        const fetchNotifications = async () => {
+            try {
+                const response = await getNotificationByUserId(userId);
+                setNotifications(response);
+            } catch (error) {
+                console.error("Erreur lors de la récupération des notifications :", error);
+            }
+        };
+        fetchNotifications();
+    }, [userId]);
+
+
 
     return (
         <div className="notification">
-            {items.map(item => (
-                <div key={item.id}>
-                    <p className="notification-date">{item.date}:</p>
-                    <p className="notification-message">{item.message}</p>
-                </div>
-            ))}
+            {notifications.length > 0 ? (
+                notifications.map((notification) => (
+                    <div key={notification.id}>
+                        <p className="notification-date">{notification.date}:</p>
+                        <p className="notification-message">{notification.message}</p>
+                    </div>
+                ))
+            ) : (
+                <p>Aucune notification</p>
+            )}
         </div>
     );
 }
